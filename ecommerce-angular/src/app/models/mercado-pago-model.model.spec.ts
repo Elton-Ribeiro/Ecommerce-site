6811f2300c3f714b62c8bc01e9d4b169c7e7ea7a
Elton-Ribeiro <ear.elton@gmail.com>
2022-05-06 11:07:39 -0300

    Interface created

--- ecommerce-angular/src/app/models/mercado-pago-model.model.spec.ts
+++
@@ -1,7 +0,0 @@
-import { MercadoPagoModel } from './mercado-pago-model.model';
-
-describe('MercadoPagoModel', () => {
-  it('should create an instance', () => {
-    expect(new MercadoPagoModel()).toBeTruthy();
-  });
-});
